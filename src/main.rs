9a845984e246dcba17974527575f72e763b32b4d
Stan-breaks <stanleymwendwa03@gmail.com>
2025-05-29 10:59:15 +0300

    simple request

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use reqwest::{self, Error};
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    let url = "http://www.rustinaction.com/";
+    let response = reqwest::get(url).await?.text().await.unwrap();
+    println!("{}", response);
+    Ok(())
 }
