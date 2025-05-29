e954b25d15251a13d3a8190d69abefa7d1f99a2b
Stan-breaks <stanleymwendwa03@gmail.com>
2025-05-29 11:21:57 +0300

    using tcp
    http get

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,17 @@
-use reqwest::{self, Error};
+use std::{
+    io::{Result, Write, copy, stdout},
+    net::TcpStream,
+};
 
-#[tokio::main]
-async fn main() -> Result<(), Error> {
-    let url = "http://www.rustinaction.com/";
-    let response = reqwest::get(url).await?.text().await.unwrap();
-    println!("{}", response);
+fn main() -> Result<()> {
+    let host = "localhost:80";
+    let mut conn = TcpStream::connect(host)?;
+
+    conn.write_all(b"GET / HTTP/1.0")?;
+    conn.write_all(b"\r\n")?;
+
+    conn.write_all(b"Host: localhost")?;
+    conn.write_all(b"\r\n")?;
+    copy(&mut conn, &mut stdout())?;
     Ok(())
 }
