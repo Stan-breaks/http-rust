62e145d7a450d7ea2775bd1f65718e4f3f6941c5
Stan-breaks <stanleymwendwa03@gmail.com>
2025-05-31 15:12:49 +0300

    mod

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,6 @@
-use std::{
-    io::{Result, Write, copy, stdout},
-    net::TcpStream,
-};
-
-fn main() -> Result<()> {
-    let host = "localhost:80";
-    let mut conn = TcpStream::connect(host)?;
-
-    conn.write_all(b"GET / HTTP/1.0")?;
-    conn.write_all(b"\r\n")?;
-
-    conn.write_all(b"Host: localhost")?;
-    conn.write_all(b"\r\n")?;
-    copy(&mut conn, &mut stdout())?;
-    Ok(())
+mod dns;
+mod ethernet;
+mod http;
+fn main() {
+    println!("Hello World!");
 }
